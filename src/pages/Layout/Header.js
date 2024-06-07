37d9e53907494c51733bb4c5ef2b61d415d67dfd
findvinu <vinodyadavhhh4@gmail.com>
2024-06-08 00:23:53 +0530

    links update

--- src/pages/Layout/Header.js
+++ src/pages/Layout/Header.js
@@ -4,10 +4,10 @@ import Logo from "../../assets/images/vinuBytes.png";
 const Header = () => {
   return (
     <div className="header">
-        <img src={Logo} alt="logo" style={{height: '30px'}} />
+      <img src={Logo} alt="logo" style={{ height: "30px" }} />
       <div className="links">
-        <Link to="/user-list">User List</Link>
-        <Link to="/add-user">Add User</Link>
+        <Link to="/redux-toolkit-user-app/user-list">User List</Link>
+        <Link to="/redux-toolkit-user-app/add-user">Add User</Link>
       </div>
     </div>
   );
