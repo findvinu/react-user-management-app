23ef4c4e76f7177033a687ec7948c21cfac0fe47
findvinu <vinodyadavhhh4@gmail.com>
2024-06-08 00:19:28 +0530

    routing-update

--- src/Routing.js
+++ src/Routing.js
@@ -6,7 +6,7 @@ import UpdateUser from "./pages/UpdateUser";
 const Routing = () => {
   return (
     <Routes>
-      <Route path="/" element={<UserList />} />
+      <Route path="/redux-toolkit-user-app/" element={<UserList />} />
       <Route path="/redux-toolkit-user-app/user-list" element={<UserList />} />
       <Route path="/redux-toolkit-user-app/add-user" element={<AddUser />} />
       <Route
